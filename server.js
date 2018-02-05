da402deeed949c522aa2a11c84369fc2167d03f5
cale312 <zikisacalesogaxa@gmail.com>
2018-02-05 11:20:31 +0200

    new routeds for liking, disliking and commenting on a blog;

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // App Routes
 app.use('/api/cale_blog/v1', getAllBlogs);
