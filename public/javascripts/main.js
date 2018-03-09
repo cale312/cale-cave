af4b06780186264c7f68114b9cabe9f8ae116700
caleoasin312 <zikisa@projectcodex.co>
2018-03-09 16:08:49 +0200

    Clean code is best code, I cleaned up some repeatition and moved some code into functions, again my ocd kicked in and had to move things around. Also added restrictions to make sure that the user does not post empty blogs or comments;

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,22 +19,14 @@ function Comments(comments, hashtags, date) {
   this.date = date;
 }
 
-// get all the data from the api function
-function getData() {
-  return $.getJSON(apiUrl);
-}
-
 // function to handle when a blog is liked
 function likeBlog(_id) {
   $.ajax(apiUrl + _id + '/like', {
     type: "POST",
     success: (result) => {
       console.log(result);
-      setTimeout( () => {
-        // location.reload();
-      }, 2100);
     }
-  })
+  });
 }
 
 // function to handle when a blog is disliked
@@ -43,16 +35,8 @@ function disLikeBlog(_id) {
     type: "POST",
     success: (result) => {
       console.log(result);
-      setTimeout( () => {
-        // location.reload();
-      }, 2100);
     }
-  })
-}
-
-// function to get all comments
-function getComments(_id) {
-  return $.getJSON(apiUrl + _id);
+  });
 }
 
 function AppViewModel() {
@@ -64,56 +48,36 @@ function AppViewModel() {
   self.loader = ko.observable('<div class="preloader-wrapper big active"><div class="spinner-layer spinner-red-only"><div class="circle-clipper left"><div class="circle"></div></div><div class="gap-patch"><div class="circle"></div></div><div class="circle-clipper right"><div class="circle"></div></div></div></div>');
   self.data = ko.observable(false);
 
-  getData()
-    .then( (result) => {
-      let blogs = [];
-      let hashtags = [];
-      let data = result.blogs;
-      console.log(data);
-      data.map( (item) => {
-        blogs.push(new Blog(item.blog, item.dateAdded, item.comments, item.likes, item.dislikes, item.hashtags, item._id));
-      });
-      console.log(blogs);
-      self.data(true);
-      self.loader('');
-      self.blogs(blogs);
-    })
-    .catch( (err) => {
-      console.log(err);
-      self.loader('<p class="red-text">oops! something went wrong!</p>');
-    })
-
-  self.post = () => {
-    let data = {
-      blog: $('.blog').val()
-    };
-    $.ajax(apiUrl, {
-      data: JSON.stringify(data),
-      type: "POST", contentType: "application/json",
-      success: (result) => {
-        location.reload();
-      }
-    });
+  // get all the data from the api function
+  function getData() {
+    return $.getJSON(apiUrl)
+      .then((result) => {
+        let blogs = [];
+        let hashtags = [];
+        let data = result.blogs;
+        console.log(data);
+        data.map((item) => {
+          blogs.push(new Blog(item.blog, item.dateAdded, item.comments, item.likes, item.dislikes, item.hashtags, item._id));
+        });
+        console.log(blogs);
+        self.data(true);
+        self.loader('');
+        self.blogs(blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+        self.loader('<p class="red-text">oops! something went wrong!</p>');
+      })
   }
 
-  self.like = (evt) => {
-    blogId = evt.id;
-    likeBlog(blogId);
-  }
-
-  self.dislike = (evt) => {
-    blogId = evt.id;
-    disLikeBlog(blogId);
-  }
-
-  self.getComments = (evt) => {
-    blogId = evt.id;
+  // function to get all comments
+  function getComments(_id) {
     let comments = [];
-    getComments(blogId)
-      .then( (data) => {
+    return $.getJSON(apiUrl + _id)
+      .then((data) => {
         let blog = data.blogs;
         if (blog.comments.length > 0) {
-          blog.comments.map( (comment) => {
+          blog.comments.map((comment) => {
             comments.push(new Comments(comment.comment, comment.hashtags, comment.dateAdded));
           });
         }
@@ -121,19 +85,63 @@ function AppViewModel() {
       })
   }
 
+  // fire the getData function to display all the blogs in the database
+  getData();
+
+  self.post = () => {
+    if (document.querySelector('.blog').value.length !== 0) {
+      let data = {
+        blog: $('.blog').val()
+      };
+      $.ajax(apiUrl, {
+        data: JSON.stringify(data),
+        type: "POST",
+        contentType: "application/json",
+        success: (result) => {
+          document.querySelector('.blog').value = '';
+          getData();
+        }
+      });
+    } else {
+      alert('the fuck dude, post something or go!')
+    }
+  }
+
+  self.like = (evt) => {
+    blogId = evt.id;
+    likeBlog(blogId);
+    getData()
+  }
+
+  self.dislike = (evt) => {
+    blogId = evt.id;
+    disLikeBlog(blogId);
+    getData()
+  }
+
+  self.getComments = (evt) => {
+    blogId = evt.id;
+    getComments(blogId)
+  }
+
   self.comment = () => {
-    let data = {
-      comment: $('.comment').val()
-    };
-    $.ajax(apiUrl + blogId + '/comment', {
-      data: JSON.stringify(data),
-      type: "POST", contentType: "application/json",
-      success: (result) => {
-        setTimeout( () => {
-          location.reload();
-        }, 2100);
-      }
-    });
+    if (document.querySelector('.comment').value.length !== 0) {
+      let data = {
+        comment: $('.comment').val()
+      };
+      $.ajax(apiUrl + blogId + '/comment', {
+        data: JSON.stringify(data),
+        type: "POST",
+        contentType: "application/json",
+        success: (result) => {
+          document.querySelector('.comment').value = '';
+          getComments(blogId);
+          getData();
+        }
+      });
+    } else {
+      alert('pleas type something');
+    }
   }
 
   self.delete = (evt) => {
@@ -141,8 +149,7 @@ function AppViewModel() {
     $.ajax(apiUrl + blogId + '/delete', {
       type: "POST",
       success: (result) => {
-        alert('blog deleted');
-        location.reload();
+        getData();
       }
     });
   }
